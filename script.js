5d4ac312bec9406c55451f5d3c30697543e60346
tcjbeee <tcjbeee@gmail.com>
2022-07-04 12:18:03 +0100

    Multiple Changes: - Added JavaScript Base Code - Added Icons Folder with Book Icon

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+
+// Book Constructor
+function Book(name, author, pages, haveRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+}
+
+// Adding Book Object to Library Array
+function addBookToLibrary() {
+    myLibrary.push(newBook);
+}
