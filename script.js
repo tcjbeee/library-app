ce5dc1914bbf1dbdb2fcfc81ca35d4b2737e79b0
tcjbeee <tcjbeee@gmail.com>
2022-07-05 17:05:15 +0100

    Multiple Changes: - Added Logic for adding New Books to App Holder - Added CSS for the Above - Tweaked some CSS to make the previous commit look nicer with new changes

--- script.js
+++ script.js
@@ -9,7 +9,52 @@ function Book(name, author, pages, haveRead) {
     this.haveRead = haveRead;
 }
 
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkein", "300", "false");
+
+const darkTower = new Book("The Dark Tower", "Stephen King", "387", "true");
+
 // Adding Book Object to Library Array
-function addBookToLibrary() {
-    myLibrary.push(newBook);
+Book.prototype.addBookToLibrary = function addBookToLibrary() {
+    myLibrary.push(this);
 }
+
+theHobbit.addBookToLibrary();
+darkTower.addBookToLibrary();
+
+let bookDisplay = document.querySelector(".appHolder");
+
+
+// Function that is run through each time a new book is added from the end of the array.
+function addNewBook(myLibrary) {
+    let newAddition = myLibrary[0];
+    myLibrary.shift();
+    let newChild = document.createElement("div");
+    newChild.className = "book";
+    bookDisplay.appendChild(newChild);
+    nameText = document.createElement("p");
+    nameText.className = "textField name";
+    titleJoin = document.createElement("p");
+    titleJoin.className = "textField join";
+    authorText = document.createElement("p");
+    authorText.className = "textField author";
+    pagesText = document.createElement("p");
+    pagesText.className = "textField pages";   
+    nameText.textContent = newAddition.name;
+    titleJoin.textContent = "by";
+    authorText.textContent = newAddition.author;
+    pagesText.textContent = "Pages: " + newAddition.pages;
+
+
+    newChild.appendChild(nameText);
+    newChild.appendChild(titleJoin);
+    newChild.appendChild(authorText);
+    newChild.appendChild(pagesText);
+
+
+}
+
+while (myLibrary.length != 0){
+    console.log(myLibrary.length)
+    addNewBook(myLibrary);
+};
+
