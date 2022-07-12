fb95a2943464be4e268f0f19da84d6a6e443f05d
tcjbeee <tcjbeee@gmail.com>
2022-07-12 13:25:11 +0100

    Added Form activated on Button

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ theHobbit.addBookToLibrary();
 darkTower.addBookToLibrary();
 
 let bookDisplay = document.querySelector(".appHolder");
+const button = document.querySelector(".addButton");
+const formDiv = document.querySelector(".formDiv");
 
 
 // Function that is run through each time a new book is added from the end of the array.
@@ -58,3 +60,60 @@ while (myLibrary.length != 0){
     addNewBook(myLibrary);
 };
 
+/*function openForm() {
+    formHolder = document.querySelector(".formDiv");
+    formHolder.name = "bookForm";
+    formHolder.method = "post";
+    //formHolder.action = addFormBook() {}
+    nameField = document.createElement("input");
+    nameField.name = "bookName";
+    nameField.value = "Test Book Name";
+    formHolder.appendChild(nameField);
+    
+} */
+
+button.addEventListener("click", () => {
+        formHolder = document.querySelector(".formDiv");
+        formHolder.name = "bookForm";
+        formHolder.method = "post";
+        //formHolder.action = addFormBook() {}
+        nameField = document.createElement("input");
+        nameField.name = "bookName";
+        nameField.placeholder = "Sherlock Holmes";
+        nameLabel = document.createElement("label");
+        nameLabel.for = "nameField";
+        nameLabel.textContent = "Book Name:";
+        formHolder.appendChild(nameLabel);
+        formHolder.appendChild(nameField);
+        authorField = document.createElement("input");
+        authorField.name = "authorName";
+        authorLabel = document.createElement("label");
+        authorLabel.for = "authorName";
+        authorLabel.textContent = "Author:";
+        formHolder.appendChild(authorLabel);
+        formHolder.appendChild(authorField);
+        pageField = document.createElement("input");
+        pageField.name = "pageNumbers";
+        pageField.type = "number";
+        pageLabel = document.createElement("label");
+        pageLabel.for = "pageNumbers";
+        pageLabel.textContent = "Number of Pages:";
+        formHolder.appendChild(pageLabel);
+        formHolder.appendChild(pageField);
+        readField = document.createElement("input");
+        readField.className = "checkBox";
+        readField.name = "haveRead";
+        readField.type = "checkBox";
+        readLabel = document.createElement("label");
+        readLabel.for = "checkBox";
+        readLabel.textContent = "Have I read this?";
+        formHolder.appendChild(readLabel);
+        formHolder.appendChild(readField);
+
+        // Submit
+        submitButton = document.createElement("input");
+        submitButton.type = "submit";
+        submitButton.className = "submitButton";
+        submitButton.textContent = "Submit Book";
+        formHolder.appendChild(submitButton);
+});
